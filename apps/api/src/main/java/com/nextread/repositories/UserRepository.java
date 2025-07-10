9966f50992199b0bac68f766e09a3654bd57e992
PDJ6975 <antrodcal@alum.us.es>
2025-07-10 17:24:56 +0200

    feat: Añadir soporte para JWT y configuración de correo electrónico, además de eliminar la clase de configuración de seguridad obsoleta

---
+++ apps/api/src/main/java/com/nextread/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.nextread.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nextread.entities.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByVerificationCode(String verificationCode);
+}
