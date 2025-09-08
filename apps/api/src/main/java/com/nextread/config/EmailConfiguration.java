1e1ac299cab55e5699c9cd4cef6623aa5fe42eb7
PDJ6975 <antrodcal@alum.us.es>
2025-09-08 15:12:28 +0200

    fix: actualizar configuración de JavaMailSender para usar SSL en lugar de STARTTLS

--- apps/api/src/main/java/com/nextread/config/EmailConfiguration.java
+++ apps/api/src/main/java/com/nextread/config/EmailConfiguration.java
@@ -21,14 +21,14 @@ public class EmailConfiguration {
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setPort(465);
         mailSender.setUsername(emailUsername);
         mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
